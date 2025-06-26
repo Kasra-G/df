94d7340b7787ccd4fb7279de77963f2dc0785585
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2025-06-26 03:45:41 -0700

    Added tmux config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,6 +3,6 @@ require "nvchad.options"
 vim.opt.colorcolumn = "120"
 vim.o.so = 10
 -- add yours here!
-
+vim.g["tmux_navigator_no_wrap"] = 1
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -19,6 +19,25 @@ return {
       vim.g.crystal_auto_format = 1
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
