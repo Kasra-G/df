2bbcd7d54afa106ed899ca38185ddd9cf1dd14ee
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2025-06-26 21:19:39 -0700

    Added lazygit

--- install.sh
+++ install.sh
@@ -38,6 +38,6 @@ echo "Stowing"
 stow .
 # Additional utilities to install
 echo "Installing additional utilities"
-brew install btop go make maven tree ripgrep
+brew install btop go make maven tree ripgrep lazygit
 echo "Installation complete, remember to create GPG Signing Key"
 source $HOME/.zshrc
