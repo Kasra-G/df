0a94afab83a8990d3793cb2c2005055a0e31f78b
Kasra Ghaffari <ghkasra@amazon.com>
2026-01-22 14:42:00 -0800

    update obsidian config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -6,12 +6,26 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  opts = {
-    workspaces = {
-      {
-        name = "default",
-        path = "~/vaults/default",
+  opts = function()
+    -- obsidian
+    vim.opt.conceallevel = 1
+
+    return {
+      workspaces = {
+        {
+          name = "default",
+          path = "~/vaults/default",
+        },
       },
-    },
-  },
+      ui = {
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+          ["!"] = { char = "", hl_group = "ObsidianImportant" },
+        }
+      }
+    }
+  end
 }
