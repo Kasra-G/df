4e60e81f71901e3683b1de1fc535d2eb330931a1
Kasra Ghaffari <ghkasra@amazon.com>
2026-04-24 02:04:42 -0700

    fix for not able to move commits up/down in lazygit with tmux/nvim

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,4 +9,5 @@ o.cmdheight = 0
 o.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 -- add yours here!
 vim.g.tmux_navigator_save_on_switch = 2
+vim.g.tmux_navigator_no_mappings = 1
 -- local o = vim.o
