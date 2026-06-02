22e3f2868b12e4b18d066678ac3eec31b83f4a97
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2026-06-01 21:25:34 -0700

    update to nvim treesitter main branch

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,24 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    -- dependencies = {"AJamesyD/tree-sitter-jsonl" },
     lazy = false,
+    branch = "main",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").install {
-        "lua",
-        "python",
-        "kotlin",
-        "java",
-        "typescript",
-        "svelte",
-        "html",
-        "css",
-        "sql",
-        "json",
-        "markdown",
-        "javascript",
-      }
-    end,
   },
 }
